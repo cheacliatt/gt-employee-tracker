c1ebfed7d7e378f9379bc9820e267f39ae6ceaa8
CheaC <cheacliatt@gmail.com>
2020-07-08 16:07:28 -0400

    Set up my questions in inquirier, made 3 functions: to add, view, and exit CLA

--- employeSeeds.sql
+++ employeSeeds.sql
@@ -6,15 +6,15 @@ USE employee_db;
 
 CREATE TABLE employee
 (
-    id INT,
-    first_name VARCHAR(40),
-    last_name VARCHAR(40),
+    id INT AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(40) NOT NULL,
+    last_name VARCHAR(40) NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE role
 (
-    id INT,
+    id INT AUTO_INCREMENT NOT NULL,
     title VARCHAR(40),
     salary DECIMAL(10, 4),
     PRIMARY KEY (id)
@@ -23,7 +23,7 @@ CREATE TABLE role
 
 CREATE TABLE department
 (
-    id INT,
+    id INT AUTO_INCREMENT NOT NULL,
     name VARCHAR(40),
     PRIMARY KEY (id)
 );
